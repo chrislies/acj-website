17d0299a1309c716cd88acf0c580155fc4990616
chrislies <clai20707@gmail.com>
2023-12-04 22:03:59 -0500

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section>
+      <div
+        className="flex justify-center bg-cover bg-no-repeat bg-center h-[30vh]"
+        style={{ backgroundImage: 'url("nyc.jpeg")' }}
+      >
+        <div className="bg-slate-700/40 backdrop-blur-sm w-full h-full flex flex-col justify-center items-center">
+          <h1 className="font-bold text-[50px] px-10 text-white">About Us</h1>
+          <div className="border-b-4 border-white w-[8%]" />
+        </div>
+      </div>
+      <div className="max-container flex flex-col items-center bg-red-500d px-20 py-10">
+        <p className="">
+          ACJ International Realty is a certified New York State and City
+          Minority Women Business Enterprise (MWBE) real estate consulting firm
+          specializing real estate services.{" "}
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default page;
