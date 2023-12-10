a2c2e1b567fee4e2e4da6737122b2bd1a2f390c9
chrislies <clai20707@gmail.com>
2023-12-10 01:20:26 -0500

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,8 +21,8 @@ const page = () => {
         {/* flex flex-col items-center */}
         <p className="">
           <strong>ACJ International Realty LLC </strong>
-          is a certified New York State and City MWBE (Minority Women Business
-          Enterprise) real estate consulting firm with over 20 years of
+          is a certified New York State and City Minority Women Business
+          Enterprise (MWBE) real estate consulting firm with over 20 years of
           experience. Our specialization in property management, leasing,
           acquisitions, and strategic development follows a client-centric
           approach tailoring services to individual needs.{" "}
@@ -37,13 +37,10 @@ const page = () => {
           success of our clients in the ever-changing real estate landscape.
         </p>
       </div>
-      <div className="companyCertificates flex flex-col items-center text-center">
-        <h4 className="uppercase pb-5 text-4xl max-sm:text-3xl font-bold">
-          Our
-          <span className="bg-clip-text text-transparent bg-gradient-to-b from-yellow-500 to-yellow-700/70">
-            {" "}
-            Services
-          </span>
+      <div className="border-b-2 border-slate-300 w-[90vw] mx-auto block" />
+      <div className="companyCertificates mt-12 flex flex-col items-center text-center">
+        <h4 className="pb-5 text-4xl max-sm:text-3xl font-bold">
+          Company Certifications  
         </h4>
         <p className="2xl:w-[38vw] max-2xl:w-[42vw] max-lg:w-[57vw] max-md:w-[65vw] max-sm:w-[85vw] text-lg max-lg:text-base max-sm:text-sm tracking-tighter">
           At <span className="font-bold">ACJ International Realty LLC</span>, we
@@ -52,11 +49,6 @@ const page = () => {
           perfect rental, or selling your property, we're here to turn your real
           estate goals into reality.
         </p>
-        <div className="mt-5 flex items-center gap-2 ">
-          <div className="border-b-2 w-[120px] border-yellow-700/70" />
-          <div className="border-b-2 w-6  border-yellow-700/70 bg-[#FEFEFE]" />
-          <div className="border-b-2 w-[120px] border-yellow-700/70" />
-        </div>
       </div>
       <div className="bg-red-500d p-10 grid 2xl:grid-cols-3 max-2xl:grid-cols-3 max-md:grid-cols-2 max-sm:flex max-sm:flex-col grid-flow-row 2xl:gap-[2rem] max-2xl:gap-[25px] max-xl:gap-[22px] max-lg:gap-[20px] max-md:gap-[25px] max-sm:gap-[20px]">
         {SERVICES.map((service) => (
@@ -69,7 +61,7 @@ const page = () => {
         ))}
       </div>
       <Link href="/contact">
-        <button className="bg-green-600 hover:bg-green-700 rounded px-6 py-2 text-white text-lg">
+        <button className="bg-green-600 hover:bg-green-700 rounded px-6 py-2 mb-12 text-white text-lg">
           Get In Touch
         </button>
       </Link>
