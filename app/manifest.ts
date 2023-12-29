ce74e8b5f6c1764e0d7c6713eda17a589721f5ba
root <root@DESKTOP-UQ99FV2>
2023-12-29 15:16:01 -0500

    new favicon sizes

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,39 +1,51 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'ACJ International Realty',
-    short_name: 'ACJ International Realty',
-    description: 'ACJ International Realty Website',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#FFFFFF',
+    name: "ACJ International Realty",
+    short_name: "ACJ International Realty",
+    description: "ACJ International Realty Website",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#FFFFFF",
     icons: [
       {
-        src: '/favicon.png',
-        sizes: '144x144',
-        type: 'image/png',
+        src: "/favicon144.png",
+        sizes: "144x144",
+        type: "image/png",
         purpose: "any",
       },
       {
-        src: '/favicon.png',
-        sizes: '144x144',
-        type: 'image/png',
+        src: "/favicon144.png",
+        sizes: "144x144",
+        type: "image/png",
         purpose: "maskable",
       },
       {
-        src: '/favicon.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/favicon192.png",
+        sizes: "192x192",
+        type: "image/png",
         purpose: "any",
       },
       {
-        src: '/favicon.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/favicon192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/favicon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/favicon.png",
+        sizes: "512x512",
+        type: "image/png",
         purpose: "maskable",
       },
     ],
-  }
+  };
 }
