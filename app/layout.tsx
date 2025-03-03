0d4abb93a68a77684c179445ee0dd175f3554054
Christopher Lai <clai20707@gmail.com>
2025-03-03 17:19:22 -0500

    Add vercel analytics API

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "ACJ International Realty LLC",
@@ -9,20 +10,15 @@ export const metadata: Metadata = {
     "ACJ International Realty LLC offers expert consulting services in the commercial real estate sector. We specialize in assisting international companies in expanding their business within New York and New Jersey.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden min-h-screen">
-          {children}
-        </main>
+        <main className="relative overflow-hidden min-h-screen">{children}</main>
         {/* <main className="">{children}</main> */}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
